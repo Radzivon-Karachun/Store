c211a6e65698ea218f1f85268036684682d4236d
Radzivon-Karachun <karachun1988@gmail.com>
2022-01-29 13:24:47 +0100

    Store

---
+++ myMobiShop/shop/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+from cart.forms import CartAddProductForm
+
+
+# Create your views here.
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    # return render(request, 'store/list.html',
+    #               {'category': category,
+    #                'categories': categories,
+    #                'products': products})
+    return render(request, 'store/home.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'store/detail.html', {'product': product,
+                                                 'cart_product_form': cart_product_form})
